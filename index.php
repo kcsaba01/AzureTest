d68b546d03c60ec2eb7ebdf81d06749d04e732bf
kcsaba01 <0811994@rgu.ac.uk>
2016-02-24 14:20:30 +0000

    lab3

---
+++ index.php
@@ -0,0 +1,20 @@
+/**
+* Created by PhpStorm.
+* User: 0811994
+* Date: 24/02/2016
+* Time: 14:17
+*/
+<!DOCTYPE html>
+<html>
+    <head>
+        </head>
+      <body>
+        <p>
+            <?php
+            echo "Hello World";
+            ?>
+        </p>
+      </body>
+</html>
+
+
