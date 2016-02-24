a136a165a3d04150d2a8955151616729bd6e24e3
kcsaba01 <0811994@rgu.ac.uk>
2016-02-24 15:11:19 +0000

    lab3

--- index.php
+++ index.php
@@ -60,10 +60,20 @@
             }
             print "After deleting Rolls";
             //section 5
+            print "Section 5 using a for loop to display the content of an array";
             print "<BR>";
             print "<BR>";
             for ($i = 0; $i<=1;$i++) {
                 echo($provisionedActivities[$i] . "  ");
+            }
+            $availability = array("Nothing is available", "Specs are available", "Mugs are available", "Specs and Sausage Rolls are available");
+            $counter = 1;
+            for ($i=0; $i<=31; $i++) {
+                $counter =$i;
+                while ($counter > 3) {
+                $counter=$counter - 4;
+                }
+                echo "It's day " . $i . " today " . $availability[$counter];
             }
             ?>
         </p>
