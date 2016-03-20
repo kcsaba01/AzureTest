782af8098ba405d6c9bc7e68ccd1ced18cf04018
d <d@d.com>
2016-03-20 15:43:21 +0000

    commit

--- index.php
+++ index.php
@@ -18,10 +18,14 @@
 
 <body>
     <header>
-    <img id="bus_logo" src="businesslogo.png" alt="NoName Company Logo"/>
+    <article>
+        <img id="bus_logo" src="businesslogo.png" alt="NoName Company Logo"/>
+    </article>
+    <article>
     <div class="Page_title">
         <H1> NoName Company</H1>
     </div>
+    </article>
     </header>
     <main>
         Main comes here
