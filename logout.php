6b2fceae31c15cc7def7802ac2f50768ca9ce400
d <d@d.com>
2016-03-20 22:07:54 +0000

    commit

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Csaba Keresztessy 0811994@rgu.ac.uk
+ * Date: 20/03/2016
+ * Time: 22:07
+ */
+session_start();
+if(session_destroy())
+{
+    header("Location: index.php");
+}
+
+?>
