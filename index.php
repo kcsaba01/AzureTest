547c7f87ea4ef12917d105b83d49e39d0ae68310
d <d@d.com>
2016-03-20 15:22:38 +0000

    commit

--- index.php
+++ index.php
@@ -15,21 +15,16 @@
 <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 
+
 <body>
-<div align="center">
-<h1 style="font-family:Cambria, 'Hoefler Text', 'Liberation Serif', Times, 'Times New Roman', serif; font-size:32px;">PHP Login Form with Session</h1>
-<div class="loginBox">
-    <h3>Login Form</h3>
-    <br><br>
-    <form method="post" action="">
-        <label>Username:</label><br>
-        <input type="text" name="username" placeholder="Username" /><br><br>
-        <label>Password:</label><br>
-        <input type="password" name="Password" placeholder="password" />  <br><br>
-        <input type="submit" name="submit" value="Login" /> 
-    </form>
-    <div class="error"><?php echo $error;?></div>
-</div>
-</div>
+    <header>
+    The header comes here
+    </header>
+    <main>
+        Main comes here
+    </main>
 </body>
+    <footer>
+        Footer comes here
+    </footer>
 </html>
