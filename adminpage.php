67a1d110773458cc09ebbd1d185cd8cc02b4f721
d <d@d.com>
2016-03-20 23:31:57 +0000

    commit

--- adminpage.php
+++ adminpage.php
@@ -37,7 +37,7 @@ else
             <br><br>
             <table border=1 style="width:200">
                 <tr>
-                    <td><a href="authorise_users.php">Authorise Users</td>
+                    <td><a href="authorise_users.php"> <H1>Authorise Users</H1></td>
                 </tr>
             </table>
 
