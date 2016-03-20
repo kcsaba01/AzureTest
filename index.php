a3d2379cee2fb9187beb0d98ff51d32776e9e9b2
d <d@d.com>
2016-03-20 19:04:58 +0000

    commit

--- index.php
+++ index.php
@@ -16,17 +16,23 @@
 </head>
 <body>
     <header>
-       <table border=1 style="width:100%; border-bottom:10px black"></table>
-         <th><img id="bus_logo" height=150 src="businesslogo.png" alt="NoName Company Logo"/></th>
-        <th><H1 align="right"><i>Welcome to NoName Company</i></H1></th>
+       <table border=1 style="width:100%; border-bottom:10px black">
+         <tr>
+             <th><img id="bus_logo" height=150 src="businesslogo.png" alt="NoName Company Logo"/></th>
+             <th><H1 align="right"><i>Welcome to NoName Company</i></H1></th>
+        </tr>
+       </table>
     </header>
     <main>
         Main comes here
     </main>
 </body>
     <footer>
-        <table border=1 style="width:100%; border-top:10px black"></table>
-               <th> Contact Helpdesk</th>
-                <th><H1 align="right"> Login/Registration page</H1></th>
+        <table border=1 style="width:100%; border-top:10px black">
+         <tr>
+            <th> Contact Helpdesk</th>
+            <th><H1 align="right"> Login/Registration page</H1></th>
+         </tr>
+        </table>
     </footer>
 </html>
