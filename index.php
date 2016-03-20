15cee061ef0fdcbd9aa28021e2884ba4ce05c8aa
d <d@d.com>
2016-03-20 18:45:37 +0000

    commit

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
        <table border=1 style="width:100%">
         <tr>
             <td><img id="bus_logo" height=150 src="businesslogo.png" alt="NoName Company Logo"/></td>
-            <td><H1 align="right">NoName Company</H1></td>
+            <td><H1 align="right"><i>NoName Company</i></H1></td>
         </tr>
        </table>
 
