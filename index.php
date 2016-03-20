e7e746e03e3bfd4cb1c56b0af7d7a7abbb18a36b
d <d@d.com>
2016-03-20 19:46:17 +0000

    commit

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
                 <br><br>
                 <form method="post" action="">
                     <label>Username:</label><br>
-                    <input type="text" name="username" placeholder="username" /><br><br>
+                    <input type="text" name="username" placeholder="username" /><br><br><br>
                     <label>Password:</label><br>
                     <input type="password" name="password" placeholder="password" />  <br><br><br><br>
                     <input type="submit" name="submit" value="Login" />
