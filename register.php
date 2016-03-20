ad1f7b87d87226596dbec2b103f9e726ed522de5
d <d@d.com>
2016-03-20 20:55:47 +0000

    commit

--- register.php
+++ register.php
@@ -18,6 +18,15 @@ include("submit.php");
 </head>
 
 <body>
+<header>
+    <table class="bottomBorder" style="width:100%">
+        <tr>
+            <th><img align="left" id="bus_logo" height=150 src="businesslogo.png" alt="NoName Company Logo"/></th>
+            <th><H1 align="right"><i>Welcome to NoName Company</i></H1></th>
+        </tr>
+    </table>
+</header>
+<main>
 <form method="post" action="">
     <fieldset>
         <legend>Registration Form</legend>
@@ -30,7 +39,7 @@ include("submit.php");
                     <div align="right"><label for="name">Username</label></div>
                 </td>
                 <td>
-                    <input name="name" type="text" class="input" size="25"  required /><?php $_POST["username"]?>
+                    <input name="name" type="text" class="input" size="25"  required />>
                 </td>
             </tr>
             <tr>
@@ -64,5 +73,15 @@ include("submit.php");
         </table>
     </fieldset>
 </form>
+</main>
 </body>
+<footer>
+    <table class="topBorder" style="width:100%">
+        <tr>
+            <td><H2 align="left"> <i>Contact Helpdesk</i></H2> <p align="left">512 University Street<br>Aberdeen AB10 7QG<br> 01224 26<b> 0000</b></p></td>
+            <td><H2 align="right"> <bi> Login/Registration page</bi></H2></td>
+        </tr>
+    </table>
+
+</footer>
 </html>
