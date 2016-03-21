e6ce4052f0816ff44cbcae4c7dab631de3ed1756
d <d@d.com>
2016-03-21 22:55:23 +0000

    commit

--- utility/authuser.php
+++ utility/authuser.php
@@ -4,7 +4,7 @@
  * User: Csaba Keresztessy <0811994@rgu.ac.uk>
  * Date: 20/03/2016
  * Time: 22:05
- * Checks if a user is already registered
+ * Authorises a user
  */
 include('connection.php');
 $msg = "";
