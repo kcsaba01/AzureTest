6402cf4020055721adaa876919bcf1f489778a6a
d <d@d.com>
2016-03-22 23:48:20 +0000

    commit

--- Bugs/submit_bug.php
+++ Bugs/submit_bug.php
@@ -27,7 +27,7 @@ if(isset($_POST["submit"]))
     }
     else
     {
-        $query = mysqli_query($db, "INSERT INTO bugs (title, bugs.desc, postDate, fixDate, userID) VALUES ('$title', '$descr', '$postdate', '$fixdate' , 131)") or die(mysqli_error($db));
+        $query = mysqli_query($db, "INSERT INTO bugs (title, bugs.desc, postDate, fixDate, fixed, userID) VALUES ('$title', '$descr', '$postdate', '$fixdate', $fixed , 131)") or die(mysqli_error($db));
         if($query)
         {
             $msg = "Thank You! The bug has been logged.";
