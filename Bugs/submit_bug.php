272dd299fed394f48e2d1be85c2a9d1ebc81e69c
d <d@d.com>
2016-03-22 23:08:05 +0000

    commit

--- Bugs/submit_bug.php
+++ Bugs/submit_bug.php
@@ -11,11 +11,11 @@ if(isset($_POST["submit"]))
 {
     $title = $_POST["title"];
     $descr = $_POST["desc"];
-    $postdate = $_POST["postdate"];
-    $fixdate = $_POST["fixdate"];
-    $fixed = $_POST["fixed"];
-    $dateObj = DateTime::createFromFormat('d/m/Y', $postdate);
-    $newpostdate = $dateObj->format('Y-m-d');
+    //$postdate = $_POST["postdate"];
+    //$fixdate = $_POST["fixdate"];
+    //$fixed = $_POST["fixed"];
+    //$dateObj = DateTime::createFromFormat('d/m/Y', $postdate);
+    //$newpostdate = $dateObj->format('Y-m-d');
         //$userid = $login_user; //this can be the logged in user
     $title = mysqli_real_escape_string($db, $title);
     $descr = mysqli_real_escape_string($db, $descr);
