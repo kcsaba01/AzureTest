bd18e48528794d3f5eadcbb52f74d0e48fe8d449
d <d@d.com>
2016-03-22 00:29:31 +0000

    commit

--- Bugs/new_bug.php
+++ Bugs/new_bug.php
@@ -12,13 +12,13 @@
 <head>
     <meta charset="utf-8">
     <title>Welcome to NoName Company</title>
-    <link rel="stylesheet" href="utility/style.css" type="text/css" />
+    <link rel="stylesheet" href="/utility/style.css" type="text/css" />
 </head>
 <body>
 <header>
     <table class="bottomBorder" style="width:100%">
         <tr>
-            <th><a href="utility/logout.php"> <img align="left" id="bus_logo" height=150 src="businesslogo.png" alt="NoName Company Logo"/></th>
+            <th><a href="/utility/logout.php"> <img align="left" id="bus_logo" height=150 src="/businesslogo.png" alt="NoName Company Logo"/></th>
             <th><H1 align="right"><i>Welcome to NoName Company</i></H1></th>
         </tr>
     </table>
@@ -30,7 +30,7 @@
     <table class="topBorder" style="width:100%">
         <tr>
             <td><H2 align="left"> <i>Contact Helpdesk</i></H2> <p align="left">512 University Street<br>Aberdeen AB10 7QG<br> 01224 26<b> 0000</b></p></td>
-            <td><H2 align="right"> <bi> x</bi></H2></td>
+            <td><H2 align="right"> <bi>Log New Bug</bi></H2></td>
         </tr>
     </table>
 </footer>
