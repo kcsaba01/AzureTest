9f2db8a223194a3308243f25a9556a773ec6e5cc
d <d@d.com>
2016-03-22 23:45:43 +0000

    commit

--- Bugs/submit_bug.php
+++ Bugs/submit_bug.php
@@ -6,7 +6,7 @@
  * Time: 22:04
  * submitting a bug to the database
  */
-$msg =$fixed;
+$msg = "$fixed";
 if(isset($_POST["submit"]))
 {
     $title = $_POST["title"];
@@ -27,7 +27,7 @@ if(isset($_POST["submit"]))
     }
     else
     {
-        $query = mysqli_query($db, "INSERT INTO bugs (title, bugs.desc, postDate, fixdate, userID) VALUES ('$title', '$descr', '$postdate', '$fixdate' , 131)") or die(mysqli_error($db));
+        $query = mysqli_query($db, "INSERT INTO bugs (title, bugs.desc, postDate, fixDate, userID) VALUES ('$title', '$descr', '$postdate', '$fixdate' , 131)") or die(mysqli_error($db));
         if($query)
         {
             $msg = "Thank You! The bug has been logged.";
