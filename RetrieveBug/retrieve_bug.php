1ad41a9c18a97fc3f100473a7037a184433a798a
d <d@d.com>
2016-03-23 22:19:55 +0000

    commit

--- RetrieveBug/retrieve_bug.php
+++ RetrieveBug/retrieve_bug.php
@@ -54,7 +54,7 @@ include("../utility/check.php");
     if(isset($_POST["submit"]))
     {
         $rbugid = $_POST["rbugid"];
-        $rsql = "SELECT bugID, title FROM bugs WHERE bugID=$rbugid";
+        $rsql = "SELECT * FROM bugs WHERE bugID=$rbugid";
         $rresult = mysqli_query($db, $rsql);
         if (mysqli_num_rows($rresult) > 0) {
             // output data of each row
