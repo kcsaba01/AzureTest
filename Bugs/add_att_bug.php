2d26063b14bb1d763a5b499e93e94eb60db87f65
d <d@d.com>
2016-03-24 23:06:51 +0000

    commit

--- Bugs/add_att_bug.php
+++ Bugs/add_att_bug.php
@@ -29,9 +29,15 @@
         <input type="submit" value="Upload Image" name="submit">
     </form>
     <?php
-    chmod("../uploads", 666);
+    chmod("../uploads", 0666);
     echo substr(sprintf('%o', fileperms('/uploads')), -4);
-    echo "done";?>
+    echo "done";
+    $filename = '../uploads';
+    if (is_writable($filename)) {
+    echo 'The file is writable';
+    } else {
+    echo 'The file is not writable';
+    };?>
 </main>
 </body>
 <footer>
