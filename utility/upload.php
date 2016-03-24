3722b5c6a3ffeed18ccff59a4d61bf02e385ee8d
d <d@d.com>
2016-03-24 23:55:27 +0000

    commit

--- utility/upload.php
+++ utility/upload.php
@@ -2,39 +2,36 @@
 $target_dir = "../uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-//if(isset($_POST["submit"])) {
-  if (mime_content_type($_FILES["fileToUpload"]["tmp_name"]) == "text/plain") {$filetypeok=1;}
-    else {$filetypeok=0;}
+$fileType = pathinfo($target_file,PATHINFO_EXTENSION);
+if(isset($_POST["submit"])) {
+//check for file content
+if (mime_content_type($_FILES["fileToUpload"]["tmp_name"]) == "text/plain") {
+  $filetypeok=1;
+  $uploadOk=1;}
+else {
+  $filetypeok=0;
+  $uploadOk=0;}
     echo $filetypeok;
     echo "<BR>";
-    //$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    /**if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
+
 // Check if file already exists
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
+if ($_FILES["fileToUpload"]["size"] > 100000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+if($fileType != "txt" && $fileType != "" ) {
+    echo "Sorry, only files with txt extension or no extension are allowed.";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
+    echo "Sorry, your file does not meet the requirements.";
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
@@ -42,5 +39,5 @@ if ($uploadOk == 0) {
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
-}*/
+}
 ?>
